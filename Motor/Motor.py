774695c6565bf38d0e519ab632abd2054dd57de4
Gummi Kalli <gummikalli@advania.is>
2018-07-30 21:28:21 +0000

    First commit MotorController and MC_Controller for testing on Raspberry

--- Motor/Motor.py
+++ Motor/Motor.py
@@ -1,2 +1,107 @@
 import RPi.GPIO as GPIO
 
+# testBit() returns a nonzero result, 2**offset, if the bit at 'offset' is one.
+
+def testBit(int_type, offset):
+    mask = 1 << offset
+    return(int_type & mask)
+
+# setBit() returns an integer with the bit at 'offset' set to 1.
+
+def setBit(int_type, offset):
+    mask = 1 << offset
+    return(int_type | mask)
+
+# clearBit() returns an integer with the bit at 'offset' cleared.
+
+def clearBit(int_type, offset):
+    mask = ~(1 << offset)
+    return(int_type & mask)
+
+# toggleBit() returns an integer with the bit at 'offset' inverted, 0 -> 1 and 1 -> 0.
+
+def toggleBit(int_type, offset):
+    mask = 1 << offset
+    return(int_type ^ mask)
+
+MOTORLATCH = 18   # 12 on arduino - green wire (* good for visual reference to use same colors or change to correct color for reference)
+MOTORCLK = 24     # 4 on arduino - purple wire
+MOTORENABLE = 23  # 7 on arduino - yellow wire
+MOTORDATA = 25    # 8 on arduino - blue wire
+
+MOTOR1_A = 2
+MOTOR1_B = 3
+MOTOR2_A = 1
+MOTOR2_B = 4
+MOTOR4_A = 0
+MOTOR4_B = 6
+MOTOR3_A = 5
+MOTOR3_B = 7
+
+latch_state = 0
+
+class AFMotorController:
+
+    TimerInitialized = 0
+
+    def __init__(self):
+        global TimerInitialized = False
+
+    def enable():
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(18, GPIO.OUT)
+        GPIO.setup(MOTORENABLE, GPIO.OUT)
+        GPIO.setup(24, GPIO.OUT)
+        GPIO.setup(25, GPIO.OUT)
+
+        latch_state = 0
+
+        latch_tx(); // reset
+
+        GPIO.output(global MOTORENABLE, 0)
+
+    def latch_tx():
+        
+        GPIO.output(MOTORLATCH, 0)
+        GPIO.output(MOTORDATA, 0)
+
+        for i in range(0, 7):
+            GPIO.output(MOTORCLK, 0)
+
+            if testBit(global latch_state, 7 - i) > 0:
+                GPIO.output(MOTORDATA, 1)
+            else:
+                GPIO.output(MOTORDATA, 0)
+
+            GPIO.output(MOTORCLK, 1)
+
+        GPIO.output(MOTORLATCH, 1)
+
+MC = AFMotorController()                
+
+class AF_DCMotor:
+
+    motornum = 0
+    pwmFreq = 0
+    
+    def __init__(self, num, freq):
+        global motornum = num
+        global pwmFreq = freq
+
+        global MC.enable()
+
+        if num == 1:
+            global latch_state &= ~testBit(global MOTOR1_A) & ~testBit(global MOTOR_B)
+            global MC.latch_tx()
+            initPWM1(freq)
+
+            #TODO if num == 2, 3 and 4
+
+    def initPWM1(freq):
+        GPIO.setup(11, GPIO.OUT)
+        GPIO.output(11, 0) #arduino 11 also, brown wire
+            
+        
+
+        
+        
